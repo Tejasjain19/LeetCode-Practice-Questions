fa2f636feedfcb731d9268e80ec8405087a60e4c
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-06-13 22:49:32 +0530

    Time: 28 ms (54.38%), Space: 8 MB (59.17%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    string minWindow(string s, string t) 
+    {
+      
+        int m=s.size(),n=t.size();
+        
+        
+        
+        unordered_map<char,int> mp;
+        
+        for(auto it : t)
+        {
+            mp[it]++;
+        }
+        
+        int i=0,j=0,ans=INT_MAX,start=0;
+        int count = mp.size();
+        
+        while(j<m)
+        {
+            mp[s[j]]--;
+            
+            if(mp[s[j]]==0)
+                count--;
+            
+            if(count==0)
+            {
+                while(count==0)
+                {
+                    if(ans>j-i+1)
+                    {
+                    ans=j-i+1;
+                    start=i;
+                    }
+                    
+                
+                mp[s[i]]++;
+                
+                if(mp[s[i]]>0)
+                    count++;
+                
+                i++;
+                
+                
+                }
+                
+            }
+            
+            
+            
+            j++;
+            
+        }
+        
+        
+        
+        
+        if(ans!=INT_MAX)
+        {
+            return s.substr(start,ans);
+        }
+        
+        
+        return "";
+        
+        
+        
+        
+    }
+};
