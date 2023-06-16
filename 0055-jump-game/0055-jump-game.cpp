ec5066474dc0d8295760e6e9823d7498981f7aae
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-06-17 00:54:49 +0530

    Time: 51 ms (99.27%), Space: 48.5 MB (25.81%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int reachable=0;
+        
+        
+        for(int i=0; i<n; i++)
+        {
+            if(reachable<i)  // index becomes greator than the reachable index
+            {
+                return false;
+            }
+            
+            reachable = max(reachable , i + nums[i]);
+            
+            
+        }
+        
+        
+        return true;
+        
+        
+        
+        
+    }
+};
