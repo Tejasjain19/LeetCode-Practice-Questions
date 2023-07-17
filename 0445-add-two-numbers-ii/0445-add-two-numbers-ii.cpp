6b61a82a36260c8f192a32415fbe086bc88cf508
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-07-17 17:28:00 +0530

    Time: 34 ms (79.05%), Space: 73.6 MB (22.79%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -1,44 +1,40 @@
-class Solution { 
+class Solution {
 public:
-    ListNode* Helper(ListNode* l1, ListNode* l2) {   // approach 2 :-
-        stack<int> stack1, stack2;
-
-        while (l1 != nullptr) {
-            stack1.push(l1->val);
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1, s2;
+        while (l1) {
+            s1.push(l1->val);
             l1 = l1->next;
         }
-
-        while (l2 != nullptr) {
-            stack2.push(l2->val);
+        while (l2) {
+            s2.push(l2->val);
             l2 = l2->next;
         }
-
-        ListNode* result = nullptr;
-        int carry = 0;
-
-        while (!stack1.empty() || !stack2.empty() || carry != 0) {
-            int digit1 = !stack1.empty() ? stack1.top() : 0;
-            int digit2 = !stack2.empty() ? stack2.top() : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode* newNode = new ListNode(digit);
-            newNode->next = result;
-            result = newNode;
-
-            if (!stack1.empty())
-                stack1.pop();
-            if (!stack2.empty())
-                stack2.pop();
+        
+        int carry = 0, first, second;
+        ListNode* head = NULL;
+        while (!s1.empty() || !s2.empty() || carry) {
+            if (!s1.empty()) {
+                first = s1.top();
+                s1.pop();
+            }
+            else
+                first = 0;
+            
+            if (!s2.empty()) {
+                second = s2.top();
+                s2.pop();
+            }
+            else
+                second = 0;
+            
+            int temp = first+second+carry;
+            ListNode* node = new ListNode(temp%10);
+            node->next = head;
+            head = node;
+            carry = temp/10;
+            
         }
-
-        return result;
-    }
-
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans = Helper(l1, l2);
-        return ans;
+        return head;
     }
 };
