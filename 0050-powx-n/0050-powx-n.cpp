dcd388803ee2dc7867c91afbd9dbcbff5db874a7
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-07-24 19:22:29 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (9.38%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double myPow(double base, int exponent) {
+       
+        double result=1;
+        result=pow(base,exponent);
+        
+
+    
+        return result;
+    }
+};
