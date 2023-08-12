b996a917bf76a8121e08ca86941fa6621a2efcd7
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-08-12 14:34:07 +0530

    Time: 5 ms (28.03%), Space: 7.9 MB (6.12%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,18 +1,52 @@
 class Solution {
 public:
-int m, n;
-vector<vector<int> > dp;
-int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-	m = size(obstacleGrid), n = size(obstacleGrid[0]);
-	dp.resize(m, vector<int>(n));
-	return solve(obstacleGrid, 0, 0);
-}
-// function to recursively explore all unique paths and store the results once calculated
-int solve(vector<vector<int>>& grid, int i, int j) {
-	if(i < 0 || j < 0 || i >= m || j >= n) return 0;    // bounds checking
-	if(grid[i][j]) return dp[i][j] = 0;                 // obstacle found at current cell
-	if(i == m - 1 && j == n - 1) return 1;              // reached bottom-right of grid ? return 1
-	if(dp[i][j]) return dp[i][j];                       // if already computed for current cell, just return the stored results
-	return dp[i][j] = solve(grid, i + 1, j) + solve(grid, i, j + 1); // recursively explore the two options available with us
-}
+    
+    int solve(vector<vector<int>>& grid, int i , int j , int m , int n ,  vector<vector<int>> &dp )
+    {
+        if(i<0 || j<0 || i>=m || j>=n)
+            return 0;
+        
+        
+        if(grid[i][j]==1)
+            return dp[i][j]=0;
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        
+        if(i==m-1 && j==n-1)
+            return dp[i][j]=1;
+        
+        
+        return dp[i][j]=solve(grid,i+1,j,m,n,dp)+solve(grid,i,j+1,m,n,dp);
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        
+        
+        return solve(obstacleGrid , 0 , 0 , m , n ,dp );
+        
+        
+        
+    }
 };
