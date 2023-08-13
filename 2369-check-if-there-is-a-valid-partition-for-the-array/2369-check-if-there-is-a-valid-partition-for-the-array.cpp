fb2045a68c847fdeb4842c8c5113f2be80c2e023
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-08-13 15:18:48 +0530

    Time: 132 ms (86.15%), Space: 92.2 MB (37.85%) - LeetHub

---
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    bool solve(vector<int>& nums , int i , vector<int> &dp)
+    {
+        int n = nums.size();
+        
+        if(i==n)
+            return true;
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        if(i+1<n && nums[i]==nums[i+1]) // 2 elements are equal 
+        {
+            if(solve(nums , i+2 , dp))
+            {
+                return dp[i]=true;
+            }
+            
+            
+            if(i+2<n && nums[i+1]==nums[i+2])
+            {
+                if(solve(nums , i+3 , dp))
+                    return dp[i]=true;
+            }          
+          
+        }
+        
+        if( i+2 < n && nums[i+2]==nums[i+1]+1 && nums[i+1]==nums[i]+1)
+        {
+            if(solve(nums, i+3 , dp))
+                return dp[i]=true;
+        }
+        
+        return dp[i]=false;
+    }
+    
+    
+    bool validPartition(vector<int>& nums) 
+    {   
+        vector<int> dp(nums.size(),-1);
+        return solve(nums , 0,dp);
+        
+    }
+};
