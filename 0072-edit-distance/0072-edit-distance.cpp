bc214db2160b9646d46d6b126b7a8290a2108317
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-08-19 03:12:41 +0530

    Time: 4 ms (95.95%), Space: 9.1 MB (39.87%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
+   
     
-    
-    int solve(int i , int j , string &p , string &q ,   vector<vector<int>> &dp)
+    int solve(int i , int j , string &s, string &t ,  vector<vector<int>> &dp )
     {
         if(i<0)
             return j+1;
@@ -13,26 +13,30 @@ public:
         if(dp[i][j]!=-1)
             return dp[i][j];
         
-        if(p[i]==q[j])
-            return dp[i][j]=solve(i-1,j-1,p,q,dp);
+        if(s[i]==t[j])
+            return dp[i][j]=solve(i-1,j-1,s,t,dp);
         
-        return dp[i][j]=1+ min(solve(i,j-1,p,q,dp) , min(solve(i-1,j,p,q,dp) , solve(i-1,j-1,p,q,dp)));
+        
+        return dp[i][j]=min(1+solve(i-1,j,s,t,dp),min(1+solve(i,j-1,s,t,dp),1+solve(i-1,j-1,s,t,dp)));
+        
+        
+        
+        
+        
     }
     
-    
-    
-    
-    
-    
-
-    
+ 
     
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
+    
+        int n=word1.size();
+        int m=word2.size();
         
         vector<vector<int>> dp(n,vector<int>(m,-1));
         
         return solve(n-1 , m-1 , word1 , word2 , dp);
+        
+        
+        
     }
 };
