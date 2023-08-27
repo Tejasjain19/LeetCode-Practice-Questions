8bd53e978fa736d7f0b129373838b63c8bb679c7
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-08-27 22:52:24 +0530

    Time: 19 ms (47.17%), Space: 10.7 MB (63.55%) - LeetHub

--- 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
+++ 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
@@ -2,29 +2,24 @@ class Solution {
 public:
     int minimumSum(int n, int target) {
         
-         int ans = 0;
+      int ans = 0 ;
         
-        if(n==1 && target==1)
-            return 1;
+      int i=1;
         
-        unordered_set<int> s;
+      unordered_set<int> s;
         
         
-        for(int i = 1; s.size()<n ; i++)
+        while(s.size()<n)
         {
-            
-            if(s.find(target - i) == s.end())
+            if(s.find(target-i)==s.end())
             {
+              s.insert(i);    
                 ans+=i;
-                s.insert(i);
             }
-            
-            
+            i++;
         }
         
         return ans;
-        
-        
         
     }
 };
