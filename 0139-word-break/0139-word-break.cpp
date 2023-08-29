0e008dc69eafcff923db3973cda0f2960d33c26e
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-08-30 02:16:24 +0530

    Time: 7 ms (68.31%), Space: 8.5 MB (75.83%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,47 +1,48 @@
 class Solution {
 public:
-  int dp[301];
-   int solve(int index , string s ,  set<string> &st )
+    
+   int solve(int i , unordered_set<string> &st , string &s , vector<int> &dp   )
     {
-        
-        if(index==s.size())
+        if(i==s.size())
             return 1;
+          
+       if(dp[i]!=-1)
+           return dp[i];
+       
+       
+       string temp="";
         
-        string temp;
-        
-       if(dp[index]!=-1)
-           return dp[index];
-        
-        for(int j = index; j<s.size(); j++)
+        for(int j=i; j<s.size(); j++)
         {
-            
             temp+=s[j];
             
             if(st.find(temp)!=st.end())
             {
-                if(solve(j+1 , s , st))
-                    return dp[index]=1;
+                if(solve(j+1,st,s,dp))
+                    return dp[i]=1;
             }
-            
         }
         
-        
-        return dp[index]=0;
+        return dp[i]=0;
         
     }
     
     
     bool wordBreak(string s, vector<string>& wordDict) {
     
-        set<string> st;
+        unordered_set<string> st;
+        
+        vector<int> dp(s.size(),-1);
+        
         
         for(auto it : wordDict)
         {
             st.insert(it);
         }
         
-        memset(dp,-1,sizeof(dp));
-       return solve(0 , s , st);
+        
+        return solve(0 , st, s ,dp);
+        
         
     }
 };
