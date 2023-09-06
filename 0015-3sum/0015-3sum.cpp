92bb1c2f4c6f84e6923d42e3cb8ab57b9a1743bd
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-09-06 21:17:12 +0530

    Time: 148 ms (61.14%), Space: 29.9 MB (40.08%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,59 +1,64 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        
-        sort(nums.begin() , nums.end());
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
+    
+        sort(nums.begin(),nums.end());
         
         vector<vector<int>> res;
         
-        int n = nums.size();
+        int n=nums.size();
         
-        for(int i=0; i<n-2 ; i++)  // move for a
+        for(int i=0; i<n-2; i++)
         {
-            if(i==0 || (i>0 && nums[i]!=nums[i-1]))
-            {
-                int low = i+1;
-                int high = n-1;
-                int sum = 0-nums[i];
-                
-                while(low<high)
-                {
-                    if(nums[low]+nums[high]==sum)
-                    {
-                        vector<int> temp;
-                      
-                        temp.push_back(nums[low]);
+            
+           if(i==0 || (i>0 && nums[i]!=nums[i-1])) 
+           {
+               
+               int low = i+1;
+               int high = n-1;
+               
+               int sum = 0-nums[i];
+               
+               while(low<high)
+               {
+                   if(nums[low]+nums[high]==sum)
+                   {
+                       vector<int> temp;
+                       
+                       temp.push_back(nums[low]);
                         temp.push_back(nums[high]);
-                          temp.push_back(nums[i]);
-                        res.push_back(temp);
-                        
-                        while(low<high && nums[low]==nums[low+1])
-                            low++;
-                        
-                            while(low<high && nums[high]==nums[high-1])
+                        temp.push_back(nums[i]);
+                       
+                       res.push_back(temp);
+                       
+                       while(low<high && nums[low]==nums[low+1])
+                           low++;
+                        while(low<high && nums[high]==nums[high-1])
                            high--;
-                        
-                        low++;
-                        high--;
-                    }
-                    
-                    
-                   else if(nums[low] + nums[high] < sum)
-                    low++;
-                    else
-                    high--;
-                }
-                
-            }
-  
+                       
+                       low++; high--;
+                   }
+                   
+                   
+                   else if(nums[low]+nums[high]<sum)
+                   {
+                       low++;
+                   }
+                   else
+                   {
+                       high--;
+                   }
+                   
+               }
+               
+           }
+             
             
         }
-        
-        
+                
         return res;
-    
-        
+   
         
     }
 };
