7f5e69106a46c781326d5c646f8e5e009e91cedc
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-09-06 21:19:47 +0530

    Time: 79 ms (82.39%), Space: 67.6 MB (87.04%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,23 +5,22 @@ public:
     
         int mx=INT_MIN,sum=0,n=nums.size();
         
+        
         for(int i=0; i<n; i++)
         {
-            
             sum+=nums[i];
             
             mx=max(mx,sum);
             
             if(sum<0)
-            sum=0;             
+                sum=0;
+            
+            
+            
             
         }
-            
         
-   return mx ;
-        
-        
-        
+      return mx;  
         
     }
 };
