3522b51bbd0b47d4750b5c7501ee9bf213d58807
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2023-10-09 14:40:18 +0530

    Time: 3 ms (91.96%), Space: 13.9 MB (25.30%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) 
+    {
+        
+        int n=nums.size();
+        
+        vector<int> ans;
+        
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]==target)
+            {
+                ans.push_back(i);
+            }
+        }
+        
+        int t=ans.size();
+        
+        if(t==0)
+        {
+            ans.push_back(-1);
+            ans.push_back(-1);
+            return ans;
+        }
+        
+        if(t==1)
+        {
+            ans.push_back(ans[0]);
+            return ans;
+        }
+        
+        vector<int> fans;
+        
+        if(t==2)
+            return ans;
+        
+        if(t>2)
+        {
+            fans.push_back(ans[0]);
+            fans.push_back(ans[t-1]);
+        }
+        
+        
+        
+        return fans;
+        
+        
+        
+        
+    }
+};
