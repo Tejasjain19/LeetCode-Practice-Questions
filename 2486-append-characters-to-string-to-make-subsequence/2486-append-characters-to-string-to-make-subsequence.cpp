22b2f358368fcacb3d3c776703c1ec12ac21fb0d
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2024-06-03 20:36:19 +0530

    Time: 22 ms (26.94%), Space: 11.9 MB (83.42%) - LeetHub

---
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) 
+    {
+        int n = s.size();
+        int m = t.size();
+        int j  = 0;
+        for(int i = 0 ; i < n && j < m ; i++)
+        {
+            if(s[i] == t[j])
+            {
+                j++;
+            }
+            
+        }
+        return (m-j);
+    }
+};
