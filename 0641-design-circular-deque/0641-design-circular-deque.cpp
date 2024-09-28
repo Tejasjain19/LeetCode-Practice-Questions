8854bc54660ea9ecace6498b16fb380789f491aa
Tejas jain <77677089+Tejasjain19@users.noreply.github.com>
2024-09-28 23:04:54 +0530

    Time: 24 ms (29.32%), Space: 22.6 MB (84.26%) - LeetHub

---
+++ 0641-design-circular-deque/0641-design-circular-deque.cpp
@@ -0,0 +1,62 @@
+class MyCircularDeque {
+public:
+    deque<int>dq;
+    int n;
+    MyCircularDeque(int k) {
+        n=k;
+    }
+    
+    bool insertFront(int value) {
+        if(dq.size()<n){
+            dq.push_front(value);
+            return 1;
+        }
+        return 0;
+    }
+    
+    bool insertLast(int value) {
+        if(dq.size()<n){
+            dq.push_back(value);
+            return 1;
+        }
+        return 0;
+    }
+    
+    bool deleteFront() {
+        if(dq.size()>0){
+            dq.pop_front();
+            return 1;
+        }
+        return 0;
+    }
+    
+    bool deleteLast() {
+        if(dq.size()>0){
+            dq.pop_back();
+            return 1;
+        }
+        return 0;
+    }
+    
+    int getFront() {
+        if(dq.size()>0){
+            return dq.front();
+        }
+        return -1;
+    }
+    
+    int getRear() {
+        if(dq.size()>0){
+            return dq.back();
+        }
+        return -1;
+    }
+    
+    bool isEmpty() {
+        return dq.empty();
+    }
+    
+    bool isFull() {
+        return dq.size()==n;
+    }
+};
